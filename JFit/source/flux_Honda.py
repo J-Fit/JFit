d8fda16529b79e08cdc402468b82736f7e01c53b
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2021-04-13 15:13:51 +0800

    compare old model

--- JFit/source/flux_Honda.py
+++ JFit/source/flux_Honda.py
@@ -7,7 +7,6 @@ date: 2021.03.23
 
 
 class flux_Honda:
-
     def __init__(self, exp_site='juno'):
         import os
         import numpy as np
@@ -49,22 +48,86 @@ class flux_Honda:
             return np.zeros_like(Enu)
 
 
-if __name__ == "__main__":
-    my_flux = flux_Honda()
-    import matplotlib.pyplot as plt
-    import numpy as np
-    plt.style.use('../Style/Paper.mplstyle')
-    
-    Enu = np.linspace(1, 10)
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description="For JUNO NMO analysis.")
+    parser.add_argument("--JUNOFlux",
+                        action="store_true",
+                        default=False,
+                        help="Show atmospheric Flux at JUNO.")
+    parser.add_argument("--INO",
+                        action="store_true",
+                        default=False,
+                        help="Show atmospheric Flux at INO.")
+    parser.add_argument("--JUNOINO",
+                        action="store_true",
+                        default=False,
+                        help="Show atmospheric Flux at diff of INO and JUNO.")
 
-    # plt.plot(Enu, my_flux.get_flavor_ratio(Enu=Enu, flavor_a=12, flavor_b=14))
-    plt.plot(Enu, my_flux.get_flux(Enu,flavor_ID=14),label=r'$\nu_{\mu}$')
-    plt.plot(Enu, my_flux.get_flux(Enu,flavor_ID=-14),label=r'$\bar{\nu}_{\mu}$')
-    plt.plot(Enu, my_flux.get_flux(Enu,flavor_ID=12),label=r'$\nu_{e}$')
-    plt.plot(Enu, my_flux.get_flux(Enu,flavor_ID=-12),label=r'$\bar{\nu}_{e}$')
+    return parser
+
+
+def ShowJUNOFlux():
+    my_juno_flux = flux_Honda()
+    Enu = np.linspace(1, 20)
+
+    # plt.plot(Enu, my_juno_flux.get_flavor_ratio(Enu=Enu, flavor_a=12, flavor_b=14))
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=14),
+             label=r'$\nu_{\mu}$')
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=-14),
+             label=r'$\bar{\nu}_{\mu}$')
+    plt.plot(Enu, my_juno_flux.get_flux(Enu, flavor_ID=12), label=r'$\nu_{e}$')
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=-12),
+             label=r'$\bar{\nu}_{e}$')
     plt.yscale('log')
     # plt.xscale('log')
     plt.xlabel('Neutrino Energy [GeV]')
     plt.ylabel(r'$(m^2\cdot sec\cdot sr\cdot GeV)^{-1}$')
     plt.legend()
     plt.show()
+
+
+def ShowJUNO_INOFLux():
+    my_juno_flux = flux_Honda()
+    my_ino_flux = flux_Honda(exp_site='ino')
+    Enu = np.linspace(1, 20,100)
+
+    # plt.plot(Enu, my_juno_flux.get_flavor_ratio(Enu=Enu, flavor_a=12, flavor_b=14))
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=14) /
+             my_ino_flux.get_flux(Enu, flavor_ID=14),
+             label=r'$\nu_{\mu}$')
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=-14) /
+             my_ino_flux.get_flux(Enu, flavor_ID=-14),
+             label=r'$\bar{\nu}_{\mu}$')
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=12) /
+             my_ino_flux.get_flux(Enu, flavor_ID=12),
+             label=r'$\nu_{e}$')
+    plt.plot(Enu,
+             my_juno_flux.get_flux(Enu, flavor_ID=-12) /
+             my_ino_flux.get_flux(Enu, flavor_ID=-12),
+             label=r'$\bar{\nu}_{e}$')
+    # plt.yscale('log')
+    # plt.xscale('log')
+    plt.xlabel('Neutrino Energy [GeV]')
+    # plt.ylabel(r'$(m^2\cdot sec\cdot sr\cdot GeV)^{-1}$')
+    plt.ylabel(r'JUNO/INO(YB)')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+    plt.style.use('../Style/Paper.mplstyle')
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.JUNOFlux:
+        ShowJUNOFlux()
+    if args.JUNOINO:
+        ShowJUNO_INOFLux()
